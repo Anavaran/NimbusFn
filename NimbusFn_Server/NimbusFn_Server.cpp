96687a33c94e571c80e3b9cb15c6611bc243a0a0
Anavaran <zyao2003@gmail.com>
2024-07-31 12:56:15 -0400

    cleaned up files

---
+++ NimbusFn_Server/NimbusFn_Server.cpp
@@ -0,0 +1,15 @@
+﻿// NimbusFn_Server.cpp : Defines the entry point for the application.
+//
+
+#include "NimbusFn_Server.h"
+
+void sayhello() {
+	std::cout << "Hello from a function" << '\n';
+}
+
+int main()
+{
+	sayhello();
+	std::cout << "Hello CMake." << std::endl;
+	return 0;
+}
